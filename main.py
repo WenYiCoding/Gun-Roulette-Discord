f78a44a6fd900ffc5e09034abf33f031939f9698
WenYiCoding <wenyicoding@gmail.com>
2024-05-26 21:51:46 +0800

    comment changes, gunShoot() change

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+#!1 Later: Seperate some function to other py file for GUI
+#!2 User can type while time.sleep
+#!3 may need seperate Main Menu from Program()
+#!4 clear command not clean becoz \r
+
 # Library imports
 import os
 import random
 import time
-
-# Global constants
-TERMINAL_WIDTH = os.get_terminal_size()[0]
-
 
 # Class construction
 class Player:
@@ -15,58 +16,51 @@ class Player:
         self.items = []
 
 # Global variables
-    #!stupid hardcoded, will modulize VVV
-player1Name = ""
-player1HP = 10
-player1Items = []
-
-player2Name = ""
-player2HP = 10
-player2Items = []
-    #!stupid hardcoded, will modulize ^^^
-
+TERMINAL_WIDTH = os.get_terminal_size()[0]
 gunBullets = []
 
 # Message Strings
 askName = "Enter player name: "
 
 # Functions
-def renderCLI(): #!Not used
-    pass
-
 def clearCLI():
     os.system('cls||clear')
 
+#> For item usage logic
 def useItem(index = 999, playerItem = []):
     pass
 
+#> For gun shooting logic
 def shootGun(targetHP, bullets = []):
     result = ""
     print("You fired a gun ( -_•)▄︻テ══━一💥")
     bulletFired = bullets.pop(0)
     print("= ⁍ ")
-    time.sleep(2) #!can type while waiting
+    time.sleep(2) #!3
     if bulletFired == 1:
-        print(targetHP)
-        
         targetHP -= 1
         result = f" 🩸 HIT"
-        
-        print(targetHP)
     elif bulletFired == 0:
         result = " ⚬ Nothing happended"
-    time.sleep(2) #!can type while waiting
-    return result
+    print(result)
+    time.sleep(2) #!3
+    return targetHP
 
+#> Whole program logic
 def program():
     clearCLI()
     player1 = Player(input("Enter player 1 name: "))
-    player1Name = input("Enter player 1 name: ")
-    #!CONTINUE
-    player2 = Player(input("Enter player 1 name: "))
-    player2Name = input("Enter player 2 name: ")
+    player2 = Player(input("Enter player 2 name: "))
 
-    while (player1HP > 0) or (player2HP > 0):
+    player1Name = player1.name
+    player1Hp = player1.hp
+    player1Items = player1.items
+
+    player2Name = player2.name
+    player2Hp = player2.hp
+    player2Items = player2.items
+
+    while (player1Hp > 0) or (player2Hp > 0):
         for i in range(random.randint(2,8)):
             gunBullets.append(random.randint(0,1))
         random.shuffle(gunBullets)
@@ -77,22 +71,22 @@ def program():
         for i in range(5,-1,-1):
             print(f"⁍⁍⁍ ▄︻テ══━一 Inserting bullets ({i})...", end="")
             print("\r", end="")
-            time.sleep(1) #!can type while waiting
+            time.sleep(1) #!3
         clearCLI() #!clear command not clean becoz \r
 
         random.shuffle(gunBullets)
         turnFlag = True
         while len(gunBullets) > 0:
                 #!stupid hardcoded, will modulize VVV
-            print(f"{player1Name}:{player1HP} | {player2Name}:{player2HP}\n")
+            print(f"{player1.name}:{player1.hp} | {player2.name}:{player2.hp}\n")
             if turnFlag:
-                print(f"< {player1Name}'s turn")
+                print(f"< {player1.name}'s turn")
                 print('='*TERMINAL_WIDTH)
-                print(f"Items = {player1Items}")
+                print(f"Items = {player1.items}")
             else:
-                print(f"> {player2Name}'s turn")
+                print(f"> {player2.name}'s turn")
                 print('='*TERMINAL_WIDTH)
-                print(f"Items = {player2Items}")
+                print(f"Items = {player2.items}")
             
             print("[G]Use gun [1~8]Use item [X]Exit")
             actionChar = input(">>> ")
@@ -100,15 +94,15 @@ def program():
 
             if actionChar == "G":
                 while True:
-                    frontName = player2Name if turnFlag else player1Name
-                    frontHP = player2HP if turnFlag else player1HP
+                    frontPlayer = player2.name if turnFlag else player1.name
+                    frontHP = player2.hp if turnFlag else player1.hp
 
-                    selfName = player1Name if turnFlag else player2Name
-                    selfHP = player1HP if turnFlag else player2HP
+                    selfPlayer = player1.name if turnFlag else player2.name
+                    selfHP = player1.hp if turnFlag else player2.hp
 
                     print("You are holding a gun ( -_•)▄︻テ══━一")
                     print('='*TERMINAL_WIDTH)
-                    actionChar = input(f"[X]Shoot front: {frontName} [O]Shoot self: {selfName}\n>>> ")
+                    actionChar = input(f"[X]Shoot front: {frontPlayer} [O]Shoot self: {selfPlayer}\n>>> ")
                     clearCLI()
 
                     if actionChar == "X" or actionChar == "O":
@@ -120,7 +114,7 @@ def program():
                     targetHP = frontHP
                 elif actionChar == "O":
                     targetHP = selfHP
-                print(shootGun(targetHP, gunBullets))
+                player1.hp = shootGun(targetHP, gunBullets)
                 clearCLI()
 
                 turnFlag = not(turnFlag)
@@ -137,16 +131,12 @@ def program():
                 print("[X] Invalid input\n")
                 #!stupid hardcoded, will modulize ^^^
     
-    if player1HP < 1:
-        print(f"☠️ {player1Name} is DEAD")
-    if player2HP < 1:
-        print(f"☠️ {player2Name} is DEAD")
+    if player1.hp < 1:
+        print(f"☠️ {player1.name} is DEAD")
+    if player2.hp < 1:
+        print(f"☠️ {player2.name} is DEAD")
 
 # MAIN
-""" program()
+program()
 while (input("Game ended! Play again? [Y/N]")):
-    program() """
-player1 = Player(input("Enter player 1 name: "))
-print(player1.name)
-print(player1.hp)
-print(player1.items)
+    program()
